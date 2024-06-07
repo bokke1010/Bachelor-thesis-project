08acb9deefe478275f35ed5a8fbd2fea942965eb
bokke1010 <bokke1010@gmail.com>
2024-06-07 18:42:38 +0200

    Add some graph generation code

---
+++ demofourier.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+res = 100
+x = np.linspace(0, 6, 6 * res, endpoint=False)
+
+# Constant frequency
+
+oy = 0.6 * np.cos(5*np.pi * x / 2.0) + 0.4 * np.cos(23*np.pi * x / 2.0)
+plt.subplot(2,2,1)
+plt.xlabel("Time")
+plt.ylabel("y")
+plt.plot(x,oy)
+
+sp = np.fft.fft(oy)
+frac = np.fft.fftfreq(6*res)
+N = 160 # Shows only a tenth of the theoretical resolution
+plt.subplot(2,2,3)
+plt.plot(2 * res * frac[:N], np.abs(sp[:N]))
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+
+# Dynamic frequency
+
+y = np.empty_like(x)
+y[:2*res] = np.cos(3*np.pi * x[:2*res] / 2.0)
+y[2*res:4*res] = -np.cos(8*np.pi * x[2*res:4*res])
+y[4*res:5*res] = -np.cos(16 * np.pi * x[4*res:5*res])
+y[5*res:] = -np.cos(32 * np.pi * x[5*res:])
+
+plt.subplot(2,2,2)
+plt.xlabel("Time")
+plt.ylabel("y")
+plt.plot(x,y)
+
+sp = np.fft.fft(y)
+frac = np.fft.fftfreq(6*res)
+N = 160 # Shows only a tenth of the theoretical resolution
+plt.subplot(2,2,4)
+plt.plot(2 * res * frac[:N],np.abs(sp[:N]))
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+
+plt.suptitle("The fourier transform for stationary and time-dependant signals.")
+plt.show()
---
+++ demowavelet.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pywt
+
+from PIL import Image
+
+import matplotlib.pyplot as plt
+
+def load_image( infilename ) :
+    img = Image.open( infilename )
+    img.load()
+    data = np.asarray( img, dtype="int32" )
+    return data
+
+wavelets = ["db3", "db7", "haar"]
+wavelet = "db2"
+reconstruct = False
+
+img_blue = load_image("muis_small.png")[:,:,2]
+(cA, (cH, cV, cD)) = pywt.dwt2(img_blue, wavelet)
+plt.suptitle(f"Discrete 2D wavelet transform of a cat picture\nusing the {wavelet} wavelet.")
+
+if reconstruct:
+    cDm = np.zeros_like(cD)
+    reconstructed = pywt.idwt2((cA, (cH, cV, cDm)), wavelet)
+    imin, imax = np.min(reconstructed), np.max(reconstructed)
+    plt.imshow((reconstructed + imin) / (imax + imin), cmap="gray")
+else:
+    for i, c in enumerate([cA, cH, cV, cD]):
+        plt.subplot(2,2,i+1)
+        imin, imax = np.min(c), np.max(c)
+        plt.imshow((c + imin) / (imax + imin), cmap="gray")
+plt.show()
