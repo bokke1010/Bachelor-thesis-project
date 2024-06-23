993182ab2738b95e3b51fa26ccaf9d4f4cfe3ca3
bokke1010 <bokke1010@gmail.com>
2024-06-23 20:31:17 +0200

    Add command line functionality to convert

--- convert.py
+++ convert.py
@@ -1,14 +1,33 @@
 import os, sys
 from PIL import Image
 
-size = (1008, 756)
+def convert(path, x, y, extension = ".JPG"):
+    """Overrides all JPG files in path with rescaled versions with resolution (x, y).
+    Uses LANCZOS resampling."""
+    
+    count, total = 0,0
 
-image_path = "B:\\Images PRNU\\Onderzoek naar foto-video-vergelijkingenlars\\Onderzoek naar foto-video-vergelijkingen\\All Images\\other_images_small"
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file and entry.name.endswith(extension):
+                im = Image.open(entry)
+                im = im.resize((x, y), Image.Resampling.LANCZOS)
+                im.save(entry)
+                count += 1
+            total += 1
+    print(f"Rescaled {count}/{total} files in the given folder.")
 
-
-with os.scandir(image_path) as it:
-    for entry in it:
-        if entry.is_file and entry.name.endswith(".JPG"):
-            im = Image.open(entry)
-            im = im.resize(size, Image.Resampling.LANCZOS)
-            im.save(entry)
+if __name__ == "__main__":
+    if len(sys.argv) < 4 or len(sys.argv) > 5:
+        print("Convert takes 3 arguments: x, y, path, [extension]")
+        exit()
+    path = sys.argv[3]
+    try:
+        x, y = int(sys.argv[1]), int(sys.argv[2])
+    except:
+        raise TypeError("Could not interpret argument types.")
+    
+    if len(sys.argv) == 5:
+        convert(path, x, y, sys.argv[4])
+    else:
+        convert(path, x, y)
